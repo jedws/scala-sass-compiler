9572e4048ceedc59c023b1b01b03cb02e0d34801
Jed Wesley-Smith <jed@atlassian.com>
2011-06-03 09:02:16 +1000

    updated to scala 2.8.1 and got the specs deps right, all compiles and passes tests

--- project/build/SassCompilerProject.scala
+++ project/build/SassCompilerProject.scala
@@ -4,7 +4,7 @@ class SassCompilerProject(info: ProjectInfo) extends DefaultProject(info)
 {
   override def compileOptions = Optimize :: Unchecked :: super.compileOptions.toList
   
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5-SNAPSHOT" % "test->default"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.7" % "test->default"
   val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.7" % "test->default"
   
   override def repositories = Set(ScalaToolsSnapshots)
